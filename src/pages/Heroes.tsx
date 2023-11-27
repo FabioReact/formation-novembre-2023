2c34ca8e4e8fa4d6db6a5db5527a2b933aec63e0
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2023-11-27 16:27:37 +0100

    feat: add useEffect for fetching heroes by selected letter

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const arrayOfLetters: string[] = [];
 for (let i = 97; i <= 122; i++) {
-    arrayOfLetters.push(String.fromCharCode(i))
+  arrayOfLetters.push(String.fromCharCode(i));
 }
 // const letters: Array<string> = []
 
 const Heroes = () => {
   const [selectedLetter, setSelectedLetter] = useState<string>("a");
+  useEffect(() => {
+    console.log("Premier rendu de Heroes - useEffect");
+  }, []);
+
+  useEffect(() => {
+    // Ici, je peux executer des fonctions contenant des effets de bord
+    console.log(
+      `Premier rendu de Heroes, ou mise à jour de selectedLetter ${selectedLetter} - useEffect [selectedLetter]`
+    );
+    fetch(`http://localhost:4000/heroes?name_like=^${selectedLetter}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, [selectedLetter]);
+  // Fonction pure
+  // Sans effet de bord
   return (
     <section>
       <h1>Heroes List</h1>
