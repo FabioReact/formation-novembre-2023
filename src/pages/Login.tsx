39f64d8c4d9a8af83e0a04e42b8689081aa51cd9
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2023-11-28 15:54:33 +0100

    feat: add zod validation to react-hook-form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,25 @@
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 type Inputs = {
   email: string
   password: string
 }
 
+const schema = z.object({
+  email: z.string().email().min(7, { message: 'Email trop court - 7 caracteres minimum' }),
+  password: z.string().min(8, { message: 'Mot de passe trop court - 8 caracteres minimum' }),
+})
+
 const Login = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>()
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  })
   const onSubmitHandler = (data: Inputs) => {
     // console.log(email, password)
     console.log(data)
@@ -22,26 +31,12 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmitHandler)}>
         <fieldset>
           <label htmlFor='email'>Email:</label>
-          <input
-            type='email'
-            id='email'
-            {...register('email', {
-              required: true,
-              minLength: 7,
-            })}
-          />
+          <input type='email' id='email' {...register('email')} />
           {errors.email && <span>This field contains some errors - {errors.email.message}</span>}
         </fieldset>
         <fieldset>
           <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='password'
-            {...register('password', {
-              required: true,
-              minLength: 8,
-            })}
-          />
+          <input type='password' id='password' {...register('password')} />
           {errors.password && (
             <span>This field contains some errors - {errors.password.message}</span>
           )}
