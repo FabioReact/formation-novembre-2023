4419961e083cebcae32e3e15cbc695c9eb7a8557
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2023-11-29 10:30:59 +0100

    refactor: switch from useState to useReducer

--- src/hooks/useCounter.tsx
+++ src/hooks/useCounter.tsx
@@ -1,31 +1,79 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
+
+type State = {
+  counter: number
+  stop: boolean
+}
+
+const initialState: State = {
+  counter: 0,
+  stop: false,
+}
+
+enum ActionTypeEnum {
+  increment = 'increment',
+  stopCounter = 'stopCounter',
+  resumeCounter = 'resumeCounter',
+}
+
+type Action = {
+  type: ActionTypeEnum
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ActionTypeEnum.increment: {
+      const newState = structuredClone(state)
+      newState.counter = newState.counter + 1
+      return newState
+    }
+    case ActionTypeEnum.stopCounter: {
+      const newState = structuredClone(state)
+      newState.stop = true
+      return newState
+    }
+    case ActionTypeEnum.resumeCounter: {
+      const newState = structuredClone(state)
+      newState.stop = false
+      return newState
+    }
+    default:
+      throw new Error('Unhandled type action')
+  }
+}
 
 const useCounter = () => {
-  const [counter, setCounter] = useState(0)
-  const [isStopped, setIsStopped] = useState(false)
+  //   const [isStopped, setIsStopped] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     const id = setInterval(() => {
-      if (!isStopped) setCounter((prevCounter) => prevCounter + 1)
+      if (!state.stop) {
+        // setCounter((prevCounter) => prevCounter + 1)
+        dispatch({ type: ActionTypeEnum.increment })
+      }
     }, 1000)
     return () => {
       clearInterval(id)
     }
-  }, [isStopped])
+  }, [state.stop])
 
   const start = () => {
-    setIsStopped(false)
+    // setIsStopped(false)
+    dispatch({ type: ActionTypeEnum.resumeCounter })
   }
 
   const stop = () => {
-    setIsStopped(true)
+    // setIsStopped(true)
+    dispatch({ type: ActionTypeEnum.stopCounter })
   }
 
-  const incremente = () => {
-    setCounter((prevCounter) => prevCounter + 1)
+  const increment = () => {
+    // setCounter((prevCounter) => prevCounter + 1)
+    dispatch({ type: ActionTypeEnum.increment })
   }
 
-  return { counter, start, stop, incremente }
+  return { counter: state.counter, start, stop, increment }
 }
 
 export { useCounter }
